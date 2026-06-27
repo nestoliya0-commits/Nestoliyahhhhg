28e49aec37d9103648b9631fcc33ad9422a9698b
nestoliya0-commits <nestoliya0@gmail.com>
2026-06-28 01:59:55 +0500

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -30,7 +30,7 @@ exports.handler = async function(event, context) {
         'Authorization': 'Bearer ' + process.env.GROQ_API_KEY
       },
       body: JSON.stringify({
-        model: 'llama3-70b-8192',
+        'llama-3.3-70b-versatile',
         max_tokens: 1000,
         messages: groqMessages
       })
